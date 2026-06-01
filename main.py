65676b8b7afc5ca143a3106faae92f7c81cf5946
ayu998 <sahilmamilwar@gmail.com>
2026-06-01 20:17:06 +0530

    addquestions , answers and options

---
+++ main.py
@@ -0,0 +1,28 @@
+# Quiz game
+import random
+
+questions = ["What is the capital of India?",
+             "Which planet is known as the Red Planet?",
+             "Who is known as the Father of the Nation in India?",
+             "Which is the largest ocean on Earth?",
+             "How many continents are there on Earth?",
+             "Which gas do plants absorb from the atmosphere?",
+             "What is the national animal of India?",
+             "Who invented the telephone?",
+             "Which is the smallest prime number?",
+             "Which river is called the Ganga of South India?"]
+
+answers = ["New Delhi", "Mars", "Mahatma Gandhi", "Pacific Ocean", "7",
+           "Carbon Dioxide", "Bengal Tiger", "Alexander Graham Bell",
+           "2", "Godavari"]
+
+options = [["A) Mumbai", "B) New Delhi", "C) Kolkata", "D) Chennai"],
+           ["A) Venus", "B) Mars", "C) Jupiter", "D) Saturn"],
+           ["A) Jawaharlal Nehru", "B) Sardar Patel", "C) Bhagat Singh", "D) Mahatma Gandhi"],
+           ["A) Atlantic Ocean", "B) Indian Ocean", "C) Arctic Ocean", "D) Pacific Ocean"],
+           ["A) 5", "B) 6", "C) 7", "D) 8"],
+           ["A) Oxygen", "B) Nitrogen", "C) Carbon Dioxide", "D) Hydrogen"],
+           ["A) Lion", "B) Elephant", "C) Tiger", "D) Bear"],
+           ["A) Thomas Edison", "B) Nikola Tesla", "C) Alexander Graham Bell", "D) Guglielmo Marconi"],
+           ["A) 1", "B) 2", "C) 3", "D) 4"],
+           ["A) Kaveri", "B) Godavari", "C) Narmada", "D) Tapti"]]
